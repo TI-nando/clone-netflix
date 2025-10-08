ef323eb81c2168109861fe6a7f955361bd7ee7f0
Fernando Henrique <fernandoagro.654@gmail.com>
2025-10-07 21:18:01 -0300

    Add movie trailer functionality and YouTube integration
    - Updated package.json to include movie-trailer and react-youtube dependencies.
    - Enhanced Row component to fetch and display movie trailers on click.
    - Implemented YouTube player with autoplay option for trailer playback.

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
 import axios from "../axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import movieTrailer from "movie-trailer";
 
 type Props = {
   title: string;
@@ -17,8 +21,17 @@ type Movie = {
   backdrop_path: string;
 };
 
+type Options = {
+  height: string;
+  width: string;
+  playerVars: {
+    autoplay: 0 | 1 | undefined;
+  };
+};
+
 export function Row({ title, fetchUrl, isLargeRow = false }: Props) {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [trailerUrl, setTrailerUrl] = useState<string | null>("");
 
   const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -28,9 +41,29 @@ export function Row({ title, fetchUrl, isLargeRow = false }: Props) {
       setMovies(request.data.results);
       return request;
     }
-
     fetchData();
   }, [fetchUrl]);
+
+  const opts: Options = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie: Movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+        .then((url: string) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error: Error) => console.log(error));
+    }
+  };
 
   return (
     <div className="row">
@@ -44,6 +77,7 @@ export function Row({ title, fetchUrl, isLargeRow = false }: Props) {
               <img
                 className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                 key={movie.id}
+                onClick={() => handleClick(movie)}
                 src={`${base_url}${
                   isLargeRow ? movie.poster_path : movie.backdrop_path
                 }`}
@@ -52,6 +86,7 @@ export function Row({ title, fetchUrl, isLargeRow = false }: Props) {
             )
         )}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }

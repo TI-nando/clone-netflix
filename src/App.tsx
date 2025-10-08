af41a7f7bc0f2acd6496a313bcefad0af1059d5c
Fernando Henrique <fernandoagro.654@gmail.com>
2025-10-08 13:07:53 -0300

    adiciona o componente Footer ao App para melhorar a estrutura da página

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Nav } from "./components/Nav";
 import { Row } from "./components/Row";
 import { Banner} from "./components/Banner";
+import { Footer } from "./components/Footer";
 import requests from "./apiConfig";
 import "./App.css";
 
@@ -23,6 +24,7 @@ function App() {
       <Row title="Filmes de Terror" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Filmes de Romance" fetchUrl={requests.fetchRomanceMovies} />
       <Row title="Documentários" fetchUrl={requests.fetchDocumentaries} />
+      <Footer />
     </div>
   );
 }
